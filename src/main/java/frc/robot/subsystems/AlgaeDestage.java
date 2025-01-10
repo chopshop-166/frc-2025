4063754fdced150ccb4468bf5b07be22f83005f6
Nin <flyingchives@gmail.com>
2025-01-10 15:40:26 -0500

    Auto stash before merge of "Alpha" and "origin/Alpha"

--- src/main/java/frc/robot/subsystems/AlgaeDestage.java
+++ src/main/java/frc/robot/subsystems/AlgaeDestage.java
@@ -3,8 +3,7 @@ package frc.robot.subsystems;
 import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
 
 import frc.robot.maps.subsystems.patterns.AlgaeDestageMap;
-import frc.robot.maps.subsystems.AlgaeDestageMap.Data;
 
-public class AlgaeDestage extends LoggedSubsystem<Data, AlgaeDestageMap> {
+public class AlgaeDestage {
 
 }
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -1,10 +1,10 @@
 package frc.robot.subsystems;
 
 import javax.xml.crypto.Data;
-
+import edu.wpi.first.wpilibj2.command.Command;
 import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
 import frc.robot.maps.subsystems.patterns.OuttakeMap;
 
-public class Outtake extends LoggedSubsystem<Data, OuttakeMapMap> {
+public class Outtake extends LoggedSubsystem<Data, OuttakeMap> {
 
 }
