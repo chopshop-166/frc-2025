2835dac9dc507174349fcd384ef1e9e03f87af3a
Nin <flyingchives@gmail.com>
2025-01-10 15:43:14 -0500

    Remove loggers

--- src/main/java/frc/robot/subsystems/AlgaeDestage.java
+++ src/main/java/frc/robot/subsystems/AlgaeDestage.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
-
 import frc.robot.maps.subsystems.patterns.AlgaeDestageMap;
 
 public class AlgaeDestage {
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -1,10 +1,11 @@
 package frc.robot.subsystems;
 
 import javax.xml.crypto.Data;
-import edu.wpi.first.wpilibj2.command.Command;
 import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
 import frc.robot.maps.subsystems.patterns.OuttakeMap;
 
-public class Outtake extends LoggedSubsystem<Data, OuttakeMap> {
+public class Outtake {
 
 }
