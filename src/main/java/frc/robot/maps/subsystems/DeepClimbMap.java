df5a3d39f68f195b85d00893398d1f4b85365863
Liam Murray <liammurray2015@icloud.com>
2025-01-22 18:07:12 -0500

    edited subsystem maps

--- src/main/java/frc/robot/maps/subsystems/DeepClimbMap.java
+++ src/main/java/frc/robot/maps/subsystems/DeepClimbMap.java
@@ -4,17 +4,21 @@ import com.chopshop166.chopshoplib.logging.DataWrapper;
 import com.chopshop166.chopshoplib.logging.LoggableMap;
 import com.chopshop166.chopshoplib.logging.data.MotorControllerData;
 import com.chopshop166.chopshoplib.motors.SmartMotorController;
+import com.chopshop166.chopshoplib.sensors.IEncoder;
+import com.chopshop166.chopshoplib.sensors.MockEncoder;
 
 public class DeepClimbMap implements LoggableMap<DeepClimbMap.Data> {
 
     public SmartMotorController motor;
+    public final IEncoder encoder;
 
     public DeepClimbMap() {
-        this(new SmartMotorController());
+        this(new SmartMotorController(), new MockEncoder());
     }
 
-    public DeepClimbMap(SmartMotorController motor) {
+    public DeepClimbMap(SmartMotorController motor, IEncoder encoder) {
         this.motor = motor;
+        this.encoder = encoder;
     }
 
     // Will add data/logging stuff when we need it
@@ -22,9 +26,11 @@ public class DeepClimbMap implements LoggableMap<DeepClimbMap.Data> {
     @Override
     public void updateData(Data data) {
         data.motor.updateData(motor);
+        data.rotationAbsAngleDegrees = encoder.getAbsolutePosition();
     }
 
     public static class Data extends DataWrapper {
         public MotorControllerData motor = new MotorControllerData();
+        public double rotationAbsAngleDegrees;
     }
 }
--- src/main/java/frc/robot/maps/subsystems/ElevatorMap.java
+++ src/main/java/frc/robot/maps/subsystems/ElevatorMap.java
@@ -4,27 +4,37 @@ import com.chopshop166.chopshoplib.logging.DataWrapper;
 import com.chopshop166.chopshoplib.logging.LoggableMap;
 import com.chopshop166.chopshoplib.logging.data.MotorControllerData;
 import com.chopshop166.chopshoplib.motors.SmartMotorController;
+import com.chopshop166.chopshoplib.sensors.IEncoder;
+import com.chopshop166.chopshoplib.sensors.MockEncoder;
 
 public class ElevatorMap implements LoggableMap<ElevatorMap.Data> {
 
-    public SmartMotorController motor;
+    public SmartMotorController leftMotor;
+    public SmartMotorController rightMotor;
+    public final IEncoder encoder;
 
     public ElevatorMap() {
-        this(new SmartMotorController());
+        this(new SmartMotorController(), new SmartMotorController(), new MockEncoder());
     }
 
-    public ElevatorMap(SmartMotorController motor) {
-        this.motor = motor;
+    public ElevatorMap(SmartMotorController leftMotor, SmartMotorController rightMotor, IEncoder encoder) {
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+        this.encoder = encoder;
     }
 
     // Will add data/logging stuff when we need it
 
     @Override
     public void updateData(Data data) {
-        data.motor.updateData(motor);
+        data.leftMotor.updateData(leftMotor);
+        data.rightMotor.updateData(rightMotor);
+        data.heightAbsInches = encoder.getAbsolutePosition();
     }
 
     public static class Data extends DataWrapper {
-        public MotorControllerData motor = new MotorControllerData();
+        public MotorControllerData leftMotor = new MotorControllerData();
+        public MotorControllerData rightMotor = new MotorControllerData();
+        public double heightAbsInches;
     }
 }
