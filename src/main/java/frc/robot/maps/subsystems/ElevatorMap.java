cc2657f6006b886cbfbcab00d9fa15b2ab54daaa
Liam Murray <liammurray2015@icloud.com>
2025-01-24 16:21:24 -0500

    Added presets and limits to the elevator map

--- src/main/java/frc/robot/maps/subsystems/ElevatorMap.java
+++ src/main/java/frc/robot/maps/subsystems/ElevatorMap.java
@@ -1,5 +1,7 @@
 package frc.robot.maps.subsystems;
 
+
+import com.chopshop166.chopshoplib.ValueRange;
 import com.chopshop166.chopshoplib.logging.DataWrapper;
 import com.chopshop166.chopshoplib.logging.LoggableMap;
 import com.chopshop166.chopshoplib.logging.data.MotorControllerData;
@@ -9,21 +11,69 @@ import com.chopshop166.chopshoplib.sensors.MockEncoder;
 
 public class ElevatorMap implements LoggableMap<ElevatorMap.Data> {
 
+    public enum ElevatorPresets {
+        OFF,
+
+        INTAKE,
+
+        SCOREL1,
+
+        SCOREL2,
+
+        SCOREL3,
+
+        STOW,
+
+        HOLD
+    }
+
+    public record ElevatorPresetValues(double intake, double scoreL1, double scoreL2, double scoreL3, double stow) {
+        public ElevatorPresetValues() {
+            this(0, 0, 0, 0, 0);
+        }
+
+        public double getValue(ElevatorPresets preset) {
+            switch (preset) {
+                case OFF:
+                    return Double.NaN;
+                case INTAKE:
+                    return intake;
+                case SCOREL1:
+                    return scoreL1;
+                case SCOREL2:
+                    return scoreL2;
+                case SCOREL3:
+                    return scoreL3;
+                case STOW:
+                    return stow;
+                default:
+                    return 0;
+            }
+        }
+    }
+
     public final SmartMotorController leftMotor;
     public final SmartMotorController rightMotor;
     public final IEncoder encoder;
     public final double conversionRate;
+    public final ElevatorPresetValues elevatorPreset;
+    public final ValueRange hardLimits;
+    public final ValueRange softLimits;
 
     public ElevatorMap() {
-        this(new SmartMotorController(), new SmartMotorController(), new MockEncoder(), 1);
+        this(new SmartMotorController(), new SmartMotorController(), new MockEncoder(), 1, new ElevatorPresetValues(),
+                new ValueRange(0, 0), new ValueRange(0, 0));
     }
 
     public ElevatorMap(SmartMotorController leftMotor, SmartMotorController rightMotor, IEncoder encoder,
-            double conversionRate) {
+            double conversionRate, ElevatorPresetValues elevatorPreset, ValueRange hardLimits, ValueRange softLimits) {
         this.leftMotor = leftMotor;
         this.rightMotor = rightMotor;
         this.encoder = encoder;
         this.conversionRate = conversionRate;
+        this.elevatorPreset = elevatorPreset;
+        this.hardLimits = hardLimits;
+        this.softLimits = softLimits;
     }
 
     @Override
