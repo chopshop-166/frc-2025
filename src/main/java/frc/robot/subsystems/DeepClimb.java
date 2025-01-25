068636a730f4c4c3c767b28635ee59a93544c7e7
CluelessSomething <dcruz0918@gmail.com>
2025-01-25 13:59:53 -0500

    Made DeepClimb

--- src/main/java/frc/robot/subsystems/DeepClimb.java
+++ src/main/java/frc/robot/subsystems/DeepClimb.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.maps.subsystems.DeepClimbMap;
 import frc.robot.maps.subsystems.DeepClimbMap.Data;
 
@@ -9,6 +10,18 @@ public class DeepClimb extends LoggedSubsystem<Data, DeepClimbMap> {
 
     public DeepClimb(DeepClimbMap deepClimbMap) {
         super(new Data(), deepClimbMap);
+    }
+
+    public Command spoolIn() {
+        return runSafe(() -> {
+            getData().motor.setpoint = 1;
+        });
+    }
+
+    public Command spoolOut() {
+        return runSafe(() -> {
+            getData().motor.setpoint = -1;
+        });
     }
 
     @Override
