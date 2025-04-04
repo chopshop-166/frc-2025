75cbe264e3381e170301c5ad5eecb800d465d3d4
Timothy Bevis <tycheetah109@gmail.com>
2025-04-04 18:59:25 -0400

    Add Alerts

--- src/main/java/frc/robot/subsystems/Mitocandria.java
+++ src/main/java/frc/robot/subsystems/Mitocandria.java
@@ -2,13 +2,24 @@ package frc.robot.subsystems;
 
 import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
 
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 import frc.robot.maps.subsystems.MitocandriaMap;
 import frc.robot.maps.subsystems.MitocandriaMap.Data;
 
 public class Mitocandria extends LoggedSubsystem<Data, MitocandriaMap> {
+
+    private Alert orangePiAlert = new Alert(
+            "Orange Pi not drawing expected power, verify that Orange Pi is plugged in.", AlertType.kError);
+
     // This class is here to trigger the updateData function on the MitocandriaMap
     public Mitocandria(MitocandriaMap midocandriaMap) {
         super(new Data(), midocandriaMap);
+    }
+
+    @Override
+    public void periodic() {
+        orangePiAlert.set(getData().usb_port_1_current < 0.5);
     }
 
     @Override
